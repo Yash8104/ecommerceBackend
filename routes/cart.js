a0907d62967e85fc7963b8b838b9d473f342f865
Yash Jain <yash.jain222@nmims.edu.in>
2023-03-19 18:54:55 +0530

    added cart viewing and removing products from cart

--- routes/cart.js
+++ routes/cart.js
@@ -5,8 +5,61 @@ const {checkAuthenticated} = require('../middleware/authUser')
 const Product = require('../models/Product')
 const User = require('../models/User')
 
-router.get('/',checkAuthenticated, (req,res)=>{
-    res.render('cart.ejs');
+router.get('/',checkAuthenticated, async(req,res)=>{
+    try {
+
+        const user = await User.findById(req.user._id).populate('cart')
+        const cart = await Cart.findById(user.cart._id).populate('products.product')
+        res.render('cart.ejs', { cart: cart});
+        
+    } catch (error) {
+        console.log(error.message)
+        res.redirect('/');
+    }
+})
+
+
+router.post('/remove/:id', checkAuthenticated, async(req,res)=>{
+    try {  
+
+        const user = await User.findById(req.user._id).populate('cart')
+        const cart = await Cart.findById(user.cart._id).populate('products.product')
+        
+    
+        let quantity = 0;
+        let index = null;
+        cart.products.forEach(function(p, i){
+            if(p.product._id == req.params.id){
+                quantity = p.quantity;
+                index = i;
+            }           
+        });
+        console.log(index)
+        console.log(quantity)
+        if(quantity == req.body.quantity){
+            // max quantity (remove product from cart fully)
+            
+            cart.totalItems -= req.body.quantity;
+            cart.totalPrice -= (cart.products[index].product.price)*req.body.quantity;
+            cart.totalShippingCost -= (cart.products[index].product.shippingCost)*req.body.quantity;
+            cart.products.splice(index,1);
+            cart.save();
+        }else{
+            // remove only req.body.quantity from cart
+
+            cart.products[index].quantity -= req.body.quantity
+            cart.totalItems -= req.body.quantity;
+            cart.totalPrice -= (cart.products[index].product.price)*req.body.quantity;
+            cart.totalShippingCost -= (cart.products[index].product.shippingCost)*req.body.quantity;
+            cart.save();
+        }
+
+        res.redirect('/cart')
+        
+    } catch (error) {
+        console.log(error.message)
+        res.redirect('/cart')
+    }
 })
 
 router.post('/:id',checkAuthenticated, async(req,res)=>{
@@ -28,11 +81,6 @@ router.post('/:id',checkAuthenticated, async(req,res)=>{
             }
         });
 
-        cart.products.forEach(p => {
-            if(p.product._id.toString() === product._id.toString()){
-                console.log(p.quantity)
-            }
-        });
         
         if(containesProduct == false)
         {
@@ -45,11 +93,13 @@ router.post('/:id',checkAuthenticated, async(req,res)=>{
         cart.save();
         user.save();
         console.log(cart)
-        res.redirect('/cart')
+        res.redirect(`/products/${req.params.id}`)
     } catch (error) {
         console.log(error.message)
-        res.redirect('/cart')
+        res.redirect('/products')
     }
 })
+
+
 
 module.exports = router;
